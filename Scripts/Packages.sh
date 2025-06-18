2ab2413ae7f5dd9ad59e95343d5b8dfb0224dc90
Marywanmary <70870062+Marywanmary@users.noreply.github.com>
2025-06-18 22:57:26 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -71,7 +71,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "taskplan" "sirpdboy/luci-app-taskplan" "master"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "zram" "foundObjects/zram-swap" "master"
+#UPDATE_PACKAGE "zram" "foundObjects/zram-swap" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
